de0b3ee84ad9c290311909efd8ea28c8e9e6eb8d
andriiKrakovskyi <andrii.krakovskyi@gmail.com>
2024-10-07 15:22:58 +0200

    ‘task_3’

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+function isEnoughCapacity(products, containerSize) {
+  const productsValues = Object.values(products);
+  let productsTotal = 0;
+
+  for (const productsValue of productsValues) {
+    productsTotal += productsValue;
+  }
+  const result = productsTotal <= containerSize ? true : false;
+  return result;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
